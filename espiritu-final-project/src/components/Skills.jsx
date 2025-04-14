65b21906ae869e1f403ac16dc8b5a826cd260ef2
qretzuisx <rhizalynespiritu@gmail.com>
2025-04-15 05:54:49 +0800

    Adding skills description

--- espiritu-final-project/src/components/Skills.jsx
+++ espiritu-final-project/src/components/Skills.jsx
@@ -5,7 +5,12 @@ const Skills = () => {
     <section className="my-12">
       <h2 className="text-3xl font-semibold text-blue-600">Skills</h2>
       <p className="text-lg mt-4">
-        I am a passionate web developer with a focus on building efficient and user-friendly applications.
+      <ul>
+                                <li>Knowledgeable with multimedia software: Adobe Photoshop, Figma.</li>
+                                <li>Knowledgeable in programming language: Python, Java, C#.</li>
+                                <li>Holder of Certificate of Competency in Developing Responsive Web Design (Web Development COC-1).</li>
+                                <li>Open to feedback and seeking opportunities to improve and grow.</li>
+                            </ul>
       </p>
     </section>
   );
